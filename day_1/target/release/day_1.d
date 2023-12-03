b126c1371702223cad37f76cdac9626b94adca8f
Sérgio Matheus <sergiomatheuss09@gmail.com>
2023-12-03 14:27:34 -0300

    Day 1 completed --release

---
+++ day_1/target/release/day_1.d
@@ -0,0 +1,1 @@
+/home/matheus/Code/Rust\ Code/advent_of_code/day_1/target/release/day_1: /home/matheus/Code/Rust\ Code/advent_of_code/day_1/src/main.rs /home/matheus/Code/Rust\ Code/advent_of_code/day_1/src/sample.txt
---
+++ day_1/target/release/deps/day_1-a01e9329ac98fdb9.d
@@ -0,0 +1,6 @@
+/home/matheus/Code/Rust Code/advent_of_code/day_1/target/release/deps/day_1-a01e9329ac98fdb9: src/main.rs src/sample.txt
+
+/home/matheus/Code/Rust Code/advent_of_code/day_1/target/release/deps/day_1-a01e9329ac98fdb9.d: src/main.rs src/sample.txt
+
+src/main.rs:
+src/sample.txt:
